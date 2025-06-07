b75902aa4ad5ff0a3fecbc741b6d0b35e152ca80
Pascal Nkornyui <86390213+0x15BA88FF@users.noreply.github.com>
2025-06-07 01:11:42 +0000

    add jwt based authentication token validator

---
+++ synapse/utils/jwt_auth.py
@@ -0,0 +1,19 @@
+import jwt
+from os import getenv
+
+from typing import List, Dict, Any
+
+JWT_SECRET: str = getenv("JWT_SECRET", "secret")
+JWT_ALGORITHMS: List = getenv("JWT_ALGORITHM", "HS256").split(" ")
+
+def verify_token(token: str) -> Dict[str, Any]:
+    """
+    Verifies a JWT token using a secret and algorithm.
+    """
+
+    try:
+        return jwt.decode(token, JWT_SECRET, algorithms=JWT_ALGORITHMS)
+    except jwt.ExpiredSignatureError:
+        return { "error": "Token expired" }
+    except jwt.InvalidTokenError:
+        return { "error": "Invalid token" }
