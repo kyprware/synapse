eaee9bc1585a0d71bcdfc6ef0f487baee237abd1
Pascal Nkornyui <86390213+0x15BA88FF@users.noreply.github.com>
2025-06-14 08:19:52 +0000

    add rpc actions types

--- synapse/types/rpc_types.py
+++ synapse/types/rpc_types.py
@@ -2,8 +2,23 @@
 Static types for RPC payload base + batch data
 """
 
+from enum import Enum
 from typing import Union, List
 from ..schemas.rpc_schema import RPCNotification, RPCRequest, RPCResponse
+
+
+class RPCAction(str, Enum):
+    # Sent to synapse
+    INBOUND_DISPATCH = "inbound_dispatch"
+    INBOUND_REQUEST = "inbound_request"
+    INBOUND_RESPONSE = "inbound_response"
+    INBOUND_NOTIFICATION = "inbound_notification"
+
+    # Sent from synapse
+    OUTBOUND_DISPATCH = "outbound_dispatch"
+    OUTBOUND_REQUEST = "outbound_request"
+    OUTBOUND_RESPONSE = "outbound_response"
+    OUTBOUND_NOTIFICATION = "outbound_notification"
 
 
 RPCData = Union[RPCRequest, RPCResponse, RPCNotification]
