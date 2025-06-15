3de22b2309fe3fa80a122adefecc40f614ebdd42
Pascal Nkornyui <86390213+0x15BA88FF@users.noreply.github.com>
2025-06-15 18:26:28 +0000

    add tool to initialize database tables

---
+++ synapse/config/db_init.py
@@ -0,0 +1,13 @@
+"""
+Initialize database and create all needed tables
+"""
+
+from .db_config import engine
+from ..models.application_model import Base
+
+def initialize_database():
+    """
+    Initialize database by creating all tables.
+    """
+
+    Base.metadata.create_all(bind=engine)
