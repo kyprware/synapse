f0829afcbfed032e63d294291938b2acdde3ef5e
Pascal Nkornyui <86390213+0x15BA88FF@users.noreply.github.com>
2025-06-15 23:28:00 +0000

    update dispatch configuration with handler modules paths

--- synapse/config/dispatch_config.py
+++ synapse/config/dispatch_config.py
@@ -5,4 +5,7 @@ JSON-RPC dispatch manager configuration
 from ..utils.dispatch_utils import DispatchManager
 
 
-dispatcher = DispatchManager()
+dispatcher = DispatchManager(handler_modules=[
+    "synapse.handlers.application_handlers",
+    "synapse.handlers.application_permission_handlers"
+])
