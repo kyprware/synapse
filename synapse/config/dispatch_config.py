adcab0a18d2f71f4f0744886e09467c10dd58f5f
Pascal Nkornyui <86390213+0x15BA88FF@users.noreply.github.com>
2025-06-27 10:31:07 +0000

    rebase: add connection handlers to dispatch registry

--- synapse/config/dispatch_config.py
+++ synapse/config/dispatch_config.py
@@ -6,6 +6,7 @@ from ..utils.dispatch_utils import DispatchManager
 
 
 dispatcher = DispatchManager(handler_modules=[
+    "synapse.handlers.connection_handlers",
     "synapse.handlers.application_handlers",
     "synapse.handlers.application_permission_handlers"
 ])
